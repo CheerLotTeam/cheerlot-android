95f5b4d48deb0740292cbd0966eac18aea02fe90
dlguszoo <lhjivy1024@gmail.com>
2026-06-29 05:17:46 +0900

    [#3] design: 디자인 시스템 컬러 추가

---
+++ app/src/main/java/com/gms/cheerlotandroid/design/color/brand/BrandColor.kt
@@ -0,0 +1,31 @@
+package com.gms.cheerlotandroid.design.color.brand
+
+import androidx.compose.ui.graphics.Color
+
+/**
+ * 쳐랏의 브랜드 컬러칩입니다.
+ *
+ * 이 파일은 원본 팔레트 값만 관리합니다.
+ * 앱에서 의미를 가진 색상 이름이 필요할 때는 `semantic/CheerLotColor`에 정의합니다.
+ */
+object BrandColor {
+    // Seam
+    val Seam100 = Color(0xFFFDE7EA)
+    val Seam200 = Color(0xFFF78896)
+    val Seam300 = Color(0xFFF3586D)
+    val Seam400 = Color(0xFFF02943)
+    val Seam500 = Color(0xFFD60F29)
+    val SeamP100 = Color(0xFFF0DBDE)
+
+    // Sky
+    val Sky100 = Color(0xFFE5F2FF)
+    val Sky200 = Color(0xFFCCE6FF)
+    val Sky300 = Color(0xFF99CCFF)
+    val Sky400 = Color(0xFF66B3FF)
+    val Sky500 = Color(0xFF3399FF)
+    val Sky600 = Color(0xFF0080FF)
+    val Sky700 = Color(0xFF0066CC)
+    val Sky800 = Color(0xFF004D99)
+    val Sky900 = Color(0xFF003366)
+    val SkyP100 = Color(0xFFDBE6F0)
+}
---
+++ app/src/main/java/com/gms/cheerlotandroid/design/color/system/SystemColor.kt
@@ -0,0 +1,10 @@
+package com.gms.cheerlotandroid.design.color.system
+
+import androidx.compose.ui.graphics.Color
+
+/**
+ * 쳐랏의 시스템 상태 컬러칩입니다.
+ */
+object SystemColor {
+    val Change = Color(0xFF0FD700)
+}
