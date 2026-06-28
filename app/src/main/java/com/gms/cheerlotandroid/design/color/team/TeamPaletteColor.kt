7fece2ca519dc85e455fe0246e092477f5d4aa6f
dlguszoo <lhjivy1024@gmail.com>
2026-06-29 05:17:46 +0900

    [#3] design: 디자인 시스템 컬러 추가

---
+++ app/src/main/java/com/gms/cheerlotandroid/design/color/team/TeamPaletteColor.kt
@@ -0,0 +1,129 @@
+package com.gms.cheerlotandroid.design.color.team
+
+import androidx.compose.ui.graphics.Color
+
+/**
+ * 쳐랏의 팀 컬러칩입니다.
+ *
+ * 이 파일은 팀별 원본 팔레트 값만 관리합니다.
+ * 팀의 primary/secondary 매핑은 `TeamColor`에서 담당합니다.
+ */
+object TeamPaletteColor {
+    // Doosan
+    val DsMidnight100 = Color(0xFFEAF0FC)
+    val DsMidnight200 = Color(0xFF9BA5CC)
+    val DsMidnight300 = Color(0xFF3C478A)
+    val DsMidnight400 = Color(0xFF090845)
+    val DsMidnight500 = Color(0xFF030330)
+    val DsMidnight600 = Color(0xFF020222)
+    val DsMidnightP100 = Color(0xFFD9D9F2)
+    val DsRed100 = Color(0xFFFDE7E8)
+    val DsRed200 = Color(0xFFFAB8BA)
+    val DsRed300 = Color(0xFFF6888D)
+    val DsRed400 = Color(0xFFEF2A31)
+    val DsRed500 = Color(0xFFA60D12)
+    val DsRed600 = Color(0xFF470508)
+    val DsRedP100 = Color(0xFFF5D6D7)
+
+    // Hanwha
+    val HhOrange100 = Color(0xFFFEEBDF)
+    val HhOrange200 = Color(0xFFFFCFB2)
+    val HhOrange300 = Color(0xFFFF9E66)
+    val HhOrange400 = Color(0xFFFF5500)
+    val HhOrange500 = Color(0xFFDB4109)
+    val HhOrange600 = Color(0xFFA63107)
+    val HhOrangeP100 = Color(0xFFF2E1D9)
+
+    // KIA
+    val KiaScarlet100 = Color(0xFFFFE0E6)
+    val KiaScarlet200 = Color(0xFFFFB3C0)
+    val KiaScarlet300 = Color(0xFFF6627C)
+    val KiaScarlet400 = Color(0xFFEE1138)
+    val KiaScarlet500 = Color(0xFFBE1130)
+    val KiaScarlet600 = Color(0xFF8F0A22)
+    val KiaScarletP100 = Color(0xFFF2D9DD)
+
+    // KT
+    val KtJetblack100 = Color(0xFFF3F2F2)
+    val KtJetblack200 = Color(0xFFE1DFE0)
+    val KtJetblack300 = Color(0xFF7E7779)
+    val KtJetblack400 = Color(0xFF222021)
+    val KtJetblack500 = Color(0xFF151414)
+    val KtJetblack600 = Color(0xFF130E0E)
+    val KtJetblackP100 = Color(0xFFE9E7E8)
+    val KtRed100 = Color(0xFFFFD7D8)
+    val KtRed200 = Color(0xFFFEA4A8)
+    val KtRed300 = Color(0xFFFD3F47)
+    val KtRed400 = Color(0xFFD6020B)
+    val KtRed500 = Color(0xFFA20108)
+    val KtRed600 = Color(0xFF6F0106)
+    val KtRedP100 = Color(0xFFF5D6D8)
+
+    // Kiwoom
+    val KwBurgundy100 = Color(0xFFFEE5EB)
+    val KwBurgundy200 = Color(0xFFFA85A3)
+    val KwBurgundy300 = Color(0xFFBF3458)
+    val KwBurgundy400 = Color(0xFF780523)
+    val KwBurgundy500 = Color(0xFF490315)
+    val KwBurgundy600 = Color(0xFF31020E)
+    val KwBurgundyP100 = Color(0xFFF2D9E0)
+
+    // LG
+    val LgRed100 = Color(0xFFFDE4EB)
+    val LgRed200 = Color(0xFFFFB1C7)
+    val LgRed300 = Color(0xFFF45882)
+    val LgRed400 = Color(0xFFD70F45)
+    val LgRed500 = Color(0xFFA70B36)
+    val LgRed600 = Color(0xFF770827)
+    val LgRedP100 = Color(0xFFF2D9E0)
+
+    // Lotte
+    val LtNavy100 = Color(0xFFE9F0FC)
+    val LtNavy200 = Color(0xFF80ADF5)
+    val LtNavy300 = Color(0xFF2450B2)
+    val LtNavy400 = Color(0xFF0E2B58)
+    val LtNavy500 = Color(0xFF091C3A)
+    val LtNavy600 = Color(0xFF050F1F)
+    val LtNavyP100 = Color(0xFFD6E2F5)
+    val LtRed100 = Color(0xFFFDE7EB)
+    val LtRed200 = Color(0xFFFAB8C3)
+    val LtRed300 = Color(0xFFF35974)
+    val LtRed400 = Color(0xFFD60F32)
+    val LtRed500 = Color(0xFFA60C27)
+    val LtRed600 = Color(0xFF77091C)
+    val LtRedP100 = Color(0xFFF5D6DC)
+
+    // NC
+    val NcDeepblue100 = Color(0xFFE9F1FB)
+    val NcDeepblue200 = Color(0xFF93B9EB)
+    val NcDeepblue300 = Color(0xFF3D81DB)
+    val NcDeepblue400 = Color(0xFF1C5198)
+    val NcDeepblue500 = Color(0xFF14396C)
+    val NcDeepblue600 = Color(0xFF0C2241)
+    val NcDeepblueP100 = Color(0xFFD6E3F5)
+    val NcGold100 = Color(0xFFF7F2ED)
+    val NcGold200 = Color(0xFFE7D9CA)
+    val NcGold300 = Color(0xFFD7BFA7)
+    val NcGold400 = Color(0xFFB88C61)
+    val NcGold500 = Color(0xFF9E7347)
+    val NcGold600 = Color(0xFF7B5937)
+    val NcGoldP100 = Color(0xFFF5E5D6)
+
+    // Samsung
+    val SsBlue100 = Color(0xFFE5EEFF)
+    val SsBlue200 = Color(0xFFB2CCFF)
+    val SsBlue300 = Color(0xFF4D89FF)
+    val SsBlue400 = Color(0xFF004EE5)
+    val SsBlue500 = Color(0xFF003CB2)
+    val SsBlue600 = Color(0xFF002B80)
+    val SsBlueP100 = Color(0xFFD9E1F2)
+
+    // SSG
+    val SsgDeepred100 = Color(0xFFFDE7EA)
+    val SsgDeepred200 = Color(0xFFFAB8C0)
+    val SsgDeepred300 = Color(0xFFF3586D)
+    val SsgDeepred400 = Color(0xFFD50F29)
+    val SsgDeepred500 = Color(0xFFA70C20)
+    val SsgDeepred600 = Color(0xFF770817)
+    val SsgDeepredP100 = Color(0xFFF2D9DC)
+}
