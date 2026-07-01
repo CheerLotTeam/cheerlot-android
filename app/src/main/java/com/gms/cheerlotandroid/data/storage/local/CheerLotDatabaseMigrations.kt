ba9ba9354397e27ebbd822bb6ec4180faccb20b0
dlguszoo <lhjivy1024@gmail.com>
2026-07-01 20:34:00 +0900

    [#4] feat: CheerLotDatabase, Room migration holder 추가

---
+++ app/src/main/java/com/gms/cheerlotandroid/data/storage/local/CheerLotDatabaseMigrations.kt
@@ -0,0 +1,7 @@
+package com.gms.cheerlotandroid.data.storage.local
+
+import androidx.room.migration.Migration
+
+object CheerLotDatabaseMigrations {
+    val all: Array<Migration> = emptyArray()
+}
