e61e4e1b664f92ab877c2e3c40b208d97a274d02
dlguszoo <lhjivy1024@gmail.com>
2026-07-08 18:03:58 +0900

    [#7] fix: CheerLotApp에서 기본 화면 제거 및 Navigator 생성과 NavHost 연결

--- app/src/main/java/com/gms/cheerlotandroid/app/CheerLotApp.kt
+++ app/src/main/java/com/gms/cheerlotandroid/app/CheerLotApp.kt
@@ -1,22 +1,16 @@
 package com.gms.cheerlotandroid.app
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.gms.cheerlotandroid.core.navigation.CheerLotNavigator
+import com.gms.cheerlotandroid.core.navigation.rememberCheerLotNavigator
 import com.gms.cheerlotandroid.design.theme.CheerLotTheme
 
 @Composable
-fun CheerLotApp() {
-    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        Text(
-            text = "Hello Android!",
-            modifier = Modifier.padding(innerPadding)
-        )
-    }
+fun CheerLotApp(
+    navigator: CheerLotNavigator = rememberCheerLotNavigator(),
+) {
+    CheerLotNavHost(navigator = navigator)
 }
 
 @Preview(showBackground = true)
